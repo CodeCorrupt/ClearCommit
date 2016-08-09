ed8cba317562fac375e3cc2f7d4c1dbe6796f83f
Tyler Hoyt <tylerhoyt93@gmail.com>
2016-08-09 14:26:47 -0400

    Moved copy of post-update till after the initial push

--- setup.sh
+++ setup.sh
@@ -13,13 +13,14 @@ mkdir "../$name.git" && cd "../$name.git"
 git init --bare
 # Sore path to this folder for remote
 fullpath="$PWD"
-# Set up post-update
-wget -qP hooks "https://raw.githubusercontent.com/CodeCorrupt/Git-To-ClearCase/master/post-update"
 
 # Set up the bare repo as the remote
 cd "../$name"
 git remote add origin "file://$fullpath"
 git push -u origin master
+
+# Set up post-update
+wget -qP "$fullpath/hooks" "https://raw.githubusercontent.com/CodeCorrupt/Git-To-ClearCase/master/post-update"
 
 # instruct the user
 echo "You're all set, Simply run the following command where you want the project folder"
