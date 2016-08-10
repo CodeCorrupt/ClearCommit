23e5fce9e34a6ae5f1c7c3cb79f6c8a3b0362e65
Tyler Hoyt <thoyt01@harris.com>
2016-08-10 11:23:17 -0400

    uses subdirectory the CC Working for CC.git Also add .gitignore got CC.git

--- setup.sh
+++ setup.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+# Constants
+GITTOCC=".gittocc"
+
 # Initialize the gir repo
 git init
+# Add .gittocc to .gitignore
+echo "$GITTOCC" >> .gitignore
 # Add all files in the snapshot
 git add -A
 # Commit all the files
-git commit --allow-empty -m "INIT - Setup Git-To-ClearCase"
+git commit -m "INIT - Setup Git-To-ClearCase"
 
-# Make the bare repo one level above
-name=${PWD##*/}
-mkdir "../$name.git" && cd "../$name.git"
+# Make the bare repo at ./.gittocc
+mkdir "./$GITTOCC" && cd "./$GITTOCC"
 git init --bare
-# Sore path to this folder for remote
-fullpath="$PWD"
 
 # Set up the bare repo as the remote
-cd "../$name"
-git remote add origin "file://$fullpath"
+cd ".."
+git remote add origin "file://$PWD/$GITTOCC"
 git push -u origin master
 
 # Set up post-update
-wget -qP "$fullpath/hooks" "https://raw.githubusercontent.com/CodeCorrupt/Git-To-ClearCase/master/post-update"
+wget -qP "$PWD/$GITTOCC/hooks" "https://raw.githubusercontent.com/CodeCorrupt/Git-To-ClearCase/master/post-receive"
 
 # instruct the user
 echo ""
@@ -30,4 +32,4 @@ echo "******************************************************"
 echo "*************   All Done! :D   ***********************"
 echo "******************************************************"
 echo "You're all set, Simply run the following command where you want the project folder"
-echo "git clone $fullpath <<your task name>>"
+echo "git clone $PWD/$GITTOCC <<your task name>>"
