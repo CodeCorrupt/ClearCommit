9945fc96072fdc85ab20a35491476439fda67db2
Tyler Hoyt <tyler.hoyt@kindlingapps.com>
2016-08-15 20:38:02 -0400

    Started conversion to Python

---
+++ GTCCConfig.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+config = {
+  "Tracking" : {
+    "refs/heads/master" : "/Users/thoyt/Documents/Programming/GTCCPlayground/Snap1",
+    "refs/heads/m2" : "/Users/thoyt/Documents/Programming/GTCCPlayground/Snap2",
+  }
+}
